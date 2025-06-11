11e009371a2f1dee545501250aee3c969fe523c1
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-06-11 15:37:23 +0200

    [ADD] owl_todo_list: backend structure

---
+++ owl_todo_list/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Owl Todo List",
+    "version": "18.0.1.0.0",
+    "summary": "Owl Todo List",
+    "depends": ["web"],
+    "author": "ForgeFlow",
+    "website": "https://gitlab.forgeflow.io/forgeflow/owl-custom-addons",
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/todo_item.xml",
+    ],
+    # "assets": {
+    #     "web.assets_backend": [
+    #         "owl_todo_list/static/src/components/**/*.js",
+    #         "owl_todo_list/static/src/components/**/*.xml",
+    #         "owl_todo_list/static/src/components/**/*.scss",
+    #     ],
+    # },
+    "installable": True,
+    "application": True,
+}
