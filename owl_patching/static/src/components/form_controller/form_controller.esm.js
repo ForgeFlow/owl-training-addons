569e49ef7c7a08865a8d5aeec0b8aa474eaf8254
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-06-12 15:42:38 +0200

    [ADD] owl_patching

---
+++ owl_patching/static/src/components/form_controller/form_controller.esm.js
@@ -0,0 +1,57 @@
+import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
+import {FormController} from "@web/views/form/form_controller";
+import {_t} from "@web/core/l10n/translation";
+import {patch} from "@web/core/utils/patch";
+import {useService} from "@web/core/utils/hooks";
+
+patch(FormController.prototype, {
+    setup() {
+        super.setup();
+        this.notification = useService("notification");
+        // We declare the notification service here
+        // dialogService is already available in original FormController
+    },
+
+    async create() {
+        const result = await super.create(...arguments);
+        this.notification.add(_t("Record created successfully!"), {
+            title: _t("Create record"),
+            type: "info",
+        });
+        return result;
+    },
+
+    async save() {
+        const result = await super.save(...arguments);
+        this.notification.add(_t("Record saved successfully!"), {
+            title: _t("Save record"),
+            type: "success",
+        });
+        return result;
+    },
+
+    async discard() {
+        this.dialogService.add(ConfirmationDialog, {
+            body: _t("Are you sure you want to discard the changes?"),
+            confirmLabel: _t("Discard"),
+            confirm: async () => {
+                await super.discard(...arguments);
+            },
+            cancel: async () => {
+                this.notification.add(_t("Changes not discarded."), {
+                    title: _t("Discard changes"),
+                    type: "warning",
+                });
+            },
+        });
+    },
+
+    async duplicateRecord() {
+        const result = await super.duplicateRecord(...arguments);
+        this.notification.add(_t("Record duplicated successfully!"), {
+            title: _t("Duplicate record"),
+            type: "warning",
+        });
+        return result;
+    },
+});
