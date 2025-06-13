1a0e5bde0e30fad9a2b721bd6fd7c95371f3485d
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-06-13 15:33:51 +0200

    [ADD] owl_s02_ex01

---
+++ owl_s02_ex01/static/src/components/number_card/number_card.esm.js
@@ -0,0 +1,10 @@
+import {Component} from "@odoo/owl";
+
+export class NumberCard extends Component {
+    static template = "owl_s02_ex01.NumberCard";
+    static props = {
+        title: {type: String},
+        subtitle: {type: String},
+        value: {type: Number},
+    };
+}
