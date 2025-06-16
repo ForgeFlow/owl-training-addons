0bfbc76d2d2e8f10668976f08029c5fe508d9243
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-06-16 13:54:50 +0200

    [ADD] owl_todo_list: backend odoo base

---
+++ owl_todo_list/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Owl Todo List",
+    "version": "18.0.1.0.0",
+    "summary": "Owl Todo List",
+    "depends": ["web"],
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/owl-training-addons",
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/todo_item.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
