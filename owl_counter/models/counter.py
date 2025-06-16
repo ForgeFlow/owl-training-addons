eec6a8c24db55c683c5ac570a43d327833d1a53a
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-06-16 14:54:12 +0200

    Start of the class

--- owl_counter/models/counter.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class Counter(models.Model):
-    _name = "my.counter"
-    _description = "Simple Counter Model"
-
-    name = fields.Char()
-    count = fields.Integer(default=0)
