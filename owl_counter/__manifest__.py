7bc5fa463cbe8fca1cebc50d627869040c5c2f4e
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-06-16 12:39:54 +0200

    [ADD] owl_counter: backend odoo base

---
+++ owl_counter/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "OWL Counter",
+    "version": "18.0.1.0.0",
+    "summary": "Owl Counter",
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/owl-training-addons",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "application": True,
+}
