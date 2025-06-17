1b1f6371548230f216ec4532b5abb9e3d082c22b
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-06-17 08:06:12 +0200

    [ADD] owl_range_widget

---
+++ owl_range_widget/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Owl Range Widget",
+    "version": "18.0.1.0.0",
+    "summary": "Owl Range Widget Creation",
+    "depends": ["web", "contacts"],
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/owl-training-addons",
+    "license": "AGPL-3",
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
