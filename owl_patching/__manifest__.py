db98f99ed184ceffc1ac709c0d15cadb49c9e6f5
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-06-17 08:44:19 +0200

    [ADD] owl_patching

---
+++ owl_patching/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Owl Patching",
+    "version": "18.0.1.0.0",
+    "summary": "Owl Patching Example",
+    "depends": ["web", "contacts"],
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/owl-training-addons",
+    "license": "AGPL-3",
+    "data": ["views/res_partner_views.xml"],
+    "installable": True,
+    "application": False,
+}
