35191853278aa9b31e24343a21ef58672354013f
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-06-17 07:47:06 +0200

    [IMP] owl_range_widget

--- owl_range_widget/static/src/components/range_field.esm.js
+++ owl_range_widget/static/src/components/range_field.esm.js
@@ -1,4 +1,4 @@
-import {Component, useState} from "@odoo/owl";
+import {Component} from "@odoo/owl";
 import {_t} from "@web/core/l10n/translation";
 import {getCurrency} from "@web/core/currency";
 import {registry} from "@web/core/registry";
@@ -6,10 +6,9 @@ import {standardFieldProps} from "@web/views/fields/standard_field_props";
 
 class RangeField extends Component {
     setup() {
-        this.state = useState({
-            range: this.props.value || 0,
-        });
         const currency_id = this.props.record.data.currency_id;
+        // Get the currency symbol from the currency_id
+        // or use empty string if unavailable
         this.currency = getCurrency?.(currency_id?.[0])?.symbol || "";
     }
 }
@@ -21,5 +20,6 @@ export const rangeField = {
 };
 
 RangeField.template = "owl_view_widget_inheritance.RangeField";
+// Pulling the reactive record system Odoo uses for fields
 RangeField.props = {...standardFieldProps};
 registry.category("fields").add("range", rangeField);
