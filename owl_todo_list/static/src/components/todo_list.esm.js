d4abc23ca95275c05e38778a106ddd3ec3555603
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-06-17 11:41:10 +0200

    [ADD] owl_todo_list: notification, action, and more functions

--- owl_todo_list/static/src/components/todo_list.esm.js
+++ owl_todo_list/static/src/components/todo_list.esm.js
@@ -9,6 +9,8 @@ export class OwlTodoList extends Component {
         this.orm = useService("orm");
         this.dialogService = useService("dialog");
         this.priorityOptions = [];
+        this.notification = useService("notification");
+        this.action = useService("action");
 
         this.state = useState({
             taskList: [],
@@ -54,6 +56,39 @@ export class OwlTodoList extends Component {
     }
     editTask(task) {
         this._openForm(task.id);
+    }
+
+    async deleteTask(task) {
+        try {
+            await this.orm.unlink(this.model, [task.id]);
+            await this.getAllTasks();
+        } catch {
+            this.notification.add("Couldn't delete task", {type: "danger"});
+        }
+    }
+
+    openInForm(task) {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: this.model,
+            res_id: task.id,
+            views: [[false, "form"]],
+            target: "current",
+        });
+    }
+
+    async toggleTask(e, task) {
+        await this.orm.write(this.model, [task.id], {completed: e.target.checked});
+        await this.getAllTasks();
+        this.notification.add(
+            e.target.checked ? "Task marked completed" : "Task marked incomplete",
+            {type: "success"}
+        );
+    }
+
+    async updateColor(e, task) {
+        await this.orm.write(this.model, [task.id], {color: e.target.value});
+        await this.getAllTasks();
     }
 
     async updatePriority(e, task) {
