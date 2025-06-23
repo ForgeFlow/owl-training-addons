8667ccdbcb63270957f304cf3b1d1535f6cb393b
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-06-23 11:28:30 +0200

    [IMP] owl_patching: patch FormController component

--- owl_patching/__manifest__.py
+++ owl_patching/__manifest__.py
@@ -7,6 +7,12 @@
     "website": "https://github.com/ForgeFlow/owl-training-addons",
     "license": "AGPL-3",
     "data": ["views/res_partner_views.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "owl_patching/static/src/components/**/*.js",
+            "owl_patching/static/src/components/**/*.xml",
+        ],
+    },
     "installable": True,
     "application": False,
 }
