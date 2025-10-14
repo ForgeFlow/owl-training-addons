a48a63319b774f0aef96fb5353bc48744552f273
Arnau <arnau.cruz@forgeflow.com>
2025-10-14 16:35:45 +0200

    owl_todo_list component and create edit button

---
+++ owl_todo_list/static/src/components/todo_list.esm.js
@@ -0,0 +1,48 @@
+import { Component, useState, onWillStart } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
+import { registry } from "@web/core/registry";
+
+export class OwlTodoList extends Component {
+    setup() {
+        this.model="todo.item";
+        this.orm=useService("orm");
+        this.dialogService=useService("dialog");
+
+        this.state = useState({
+            taskList: [],
+        });
+
+        onWillStart( async()=> {
+            await this.getAllTasks();
+        })
+    }
+
+    async getAllTasks() {
+        this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "completed", "color", "priority"]);
+    }
+    async _openForm(resid=false){
+        const result = await new Promise((resolve) => {
+            this.dialogService.add(
+                FormViewDialog,
+                {
+                    resModel: this.model,
+                    resId:resid,
+                    title: resid ? "Edit Task" : "New Task",
+                    preventCreate: false,
+                    onRecordSaved: () => resolve(true),
+                },
+                {onClose: () => resolve(false)}
+            );
+        });
+        if (result) await this.getAllTasks();
+    }
+    addTask() {
+        this._openForm();
+    }
+    editTask(task) {
+        this._openForm(task.id);
+    }
+}
+OwlTodoList.template = "owl_todo_list.OwlTodoList";
+registry.category("actions").add("owl_todo_list.action_todo_list_js", OwlTodoList)
