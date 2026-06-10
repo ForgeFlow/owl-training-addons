1d47b5f0e38f6f82fcb0f5ea9f6de28c98255586
Arnau <arnau.cruz@forgeflow.com>
2026-06-10 11:41:26 +0200

    create subcomponent

--- owl_todo_list/static/src/components/todo_list.esm.js
+++ owl_todo_list/static/src/components/todo_list.esm.js
@@ -2,7 +2,7 @@ import { Component, useState, onWillStart } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
-
+import { Selector } from "./selector/selector.esm";
 export class OwlTodoList extends Component {
 
     setup() {
@@ -78,4 +78,5 @@ export class OwlTodoList extends Component {
     }
 }
 OwlTodoList.template = "owl_todo_list.OwlTodoList";
+OwlTodoList.components = { Selector };
 registry.category("actions").add("owl_todo_list.action_todo_list_js", OwlTodoList);
